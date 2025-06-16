4ebc9d3e690b1d0c6cb7bdbae5872dbc83f2024c
Amanpreet Singh <amansg421@gmail.com>
2025-06-16 20:28:46 +0530

    updated redux configuration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import { useEffect } from "react";
-import User from "./features/user/User";
-import { asyncGetUsers } from "./app/userActions";
-import { useDispatch } from "react-redux";
 import MainRoutes from "./routes/MainRoutes";
 import Nav from "./components/Nav";
 
 const App = () => {
-  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(asyncGetUsers());
+   
   }, []);
 
   return (
