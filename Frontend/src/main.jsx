8546b1aa3a1c8e54c422e0e10c134a0868e11155
Amanpreet Singh <amansg421@gmail.com>
2025-06-18 00:19:16 +0530

    centered toast notification

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -12,6 +12,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    <ToastContainer />
+    <ToastContainer  position='top-center'/>
   </Provider>
 )
