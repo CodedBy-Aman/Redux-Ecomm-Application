6991556770f9ae8bca88f1d714c2a4b96ebaff68
Amanpreet Singh <amansg421@gmail.com>
2025-06-17 23:06:45 +0530

    wrapped application with store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import { useEffect } from "react";
+
 import MainRoutes from "./routes/MainRoutes";
 import Nav from "./components/Nav";
 
 const App = () => {
 
-  useEffect(() => {
-   
-  }, []);
 
   return (
     <div className="w-screen h-screen ">
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,18 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App.jsx'
-import { store } from './app/store.jsx'
+import store from './app/store.jsx'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <ToastContainer>
     <BrowserRouter>
-    <App  />
+    <App />
     </BrowserRouter>
+    </ToastContainer>
   </Provider>
 )
