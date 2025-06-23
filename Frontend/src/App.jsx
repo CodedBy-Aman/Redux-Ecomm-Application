6d8021fc03b2f5ede7401bd73794172bfbf7ca2b
Amanpreet Singh <amansg421@gmail.com>
2025-06-23 14:57:28 +0530

    added useEffect for calling asyncGetCurrentUser

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
 
 import MainRoutes from "./routes/MainRoutes";
 import Nav from "./components/Nav";
+import { useEffect } from "react";
+import { asyncGetCurrentUser } from "./features/actions/userAction";
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
+const dispatch = useDispatch();
+const isLoggedIn = useSelector((state) => state.users.isLoggedIn)
 
+useEffect(() =>{
+  dispatch(asyncGetCurrentUser())
+},[isLoggedIn])
 
   return (
     <div className="w-screen h-screen ">
