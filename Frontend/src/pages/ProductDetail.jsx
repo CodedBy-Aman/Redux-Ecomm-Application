4922978cf78f7a284c4b9588bce8263a8020b08a
Amanpreet Singh <amansg421@gmail.com>
2025-06-28 15:31:00 +0530

    implemeted conditional rendering using isAdmin key

--- Frontend/src/pages/ProductDetail.jsx
+++ Frontend/src/pages/ProductDetail.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
-import SingleProductDetail from '../components/SingleProductDetail';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import SingleProductDetail from "../components/SingleProductDetail";
+import SingleProductUpdate from "../components/SingleProductUpdate";
 
 const ProductDetail = () => {
-const {id} = useParams();
-const products = useSelector((state) => state.products?.products|| false);
+  const isAdmin = useSelector(
+    (state) => state.users?.currentUser?.isAdmin || false
+  );
+  const { id } = useParams();
+  const products = useSelector((state) => state.products?.products || false);
 
-const selectedProduct = products?.find((product) => product.id === id);
-console.log(selectedProduct);
-
+  const selectedProduct = products?.find((product) => product.id === id);
+  console.log(selectedProduct);
 
   return (
-    <div className="p-4">
-    <SingleProductDetail product={selectedProduct}/>
+    <div className="w-screen p-4 flex justify-between gap-5">
+      <SingleProductDetail product={selectedProduct} />
+      {isAdmin && <SingleProductUpdate product={selectedProduct} />}
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
