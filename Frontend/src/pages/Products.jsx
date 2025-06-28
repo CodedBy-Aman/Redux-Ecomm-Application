e636545f548114dc4947d4dee3de4523f210cf85
Amanpreet Singh <amansg421@gmail.com>
2025-06-28 13:45:02 +0530

    added navigation route in the product card

--- Frontend/src/pages/Products.jsx
+++ Frontend/src/pages/Products.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Products = () => {
 
      const products = useSelector((state) => state.products?.products|| false);
+     const navigate = useNavigate();
+
+     const producthandler = (id) =>{
+      navigate(`/products/${id}`)
+     };
      
   return (
-       <div className="p-6">
+       <div className="p-6" >
       <h2 className="text-2xl font-semibold mb-4">All Products</h2>
 
       {products.length === 0 ? (
@@ -16,16 +22,18 @@ const Products = () => {
           {products.map((product) => (
             <div
               key={product.id}
+               onClick={() => producthandler(product.id)}
               className="bg-white border border-gray-200 rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-200"
             >
               <img
                 src={product.imageUrl}
                 alt={product.title}
+               
                 className="h-50  object-cover rounded-md mb-3 mx-auto"
               />
-              <h3 className="text-lg font-semibold">{product.title}</h3>
+              <h3 className="text-lg font-semibold" >{product.title}</h3>
               <p className="text-gray-700 text-sm line-clamp-2">{product.description}</p>
-              <p className="text-blue-600 font-bold mt-2">₹{product.price}</p>
+              <p className="text-blue-600 font-bold mt-2" >₹{product.price}</p>
               <p className="text-xs text-gray-500 mt-1">{product.category}</p>
             </div>
           ))}
