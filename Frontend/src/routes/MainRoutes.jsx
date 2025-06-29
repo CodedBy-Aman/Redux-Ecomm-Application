03bc750b81a86000b31d0cbe077af738ab918c43
Amanpreet Singh <amansg421@gmail.com>
2025-06-29 14:55:27 +0530

    implemented wrapper on routes

--- Frontend/src/routes/MainRoutes.jsx
+++ Frontend/src/routes/MainRoutes.jsx
@@ -1,26 +1,56 @@
-
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Products from '../pages/Products'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import CreateProduct from '../pages/admin/CreateProduct'
-import UpdateProduct from '../pages/admin/UpdateProduct'
-import ProductDetail from '../pages/ProductDetail'
-
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import CreateProduct from "../pages/admin/CreateProduct";
+import ProductDetail from "../pages/ProductDetail";
+import UserProfile from "../pages/users/UserProfile";
+import AuthWrapper from "../Wrapper/AuthWrapper";
+import AdminWrapper from "../Wrapper/AdminWrapper";
+import GuestWrapper from "../Wrapper/GuestWrapper";
 
 const MainRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/products' element={<Products/>} />
-        <Route path='/products/:id' element={<ProductDetail/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/admin/create-product' element={<CreateProduct/>} />
-        <Route path='/admin/update-product' element={<UpdateProduct/>} />
-    </Routes>
-  )
-}
+      <Route path="/" element={<Products />} />
+      <Route path="/products/:id" element={<ProductDetail />} />
+      <Route
+        path="/login"
+        element={
+          <GuestWrapper>
+            <Login />
+          </GuestWrapper>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <GuestWrapper>
+            <Register />
+          </GuestWrapper>
+        }
+      />
 
-export default MainRoutes
+      <Route
+        path="/user-profile"
+        element={
+          <AuthWrapper>
+            <UserProfile />
+          </AuthWrapper>
+        }
+      />
+
+      <Route
+        path="/admin/create-product"
+        element={
+          <AdminWrapper>
+            <CreateProduct />
+          </AdminWrapper>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
