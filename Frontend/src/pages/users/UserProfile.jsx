9263909f079278dbb352f7ba9d1291f147eaa45a
Amanpreet Singh <amansg421@gmail.com>
2025-06-29 14:08:34 +0530

    created user profile page

---
+++ Frontend/src/pages/users/UserProfile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import UserReadDetail from "../../components/UserReadDetail";
+import UserUpdateDetail from "../../components/UserUpdateDetail";
+import { useSelector } from "react-redux";
+
+const UserProfile = () => {
+ const user = useSelector((state) => state.users?.currentUser || false);
+
+    if (!user) {
+    return <p className="text-3xl mt-[15%] text-center text-gray-500">user not found.</p>;
+  }
+  return (
+    <div className="w-screen p-4 flex justify-between gap-5">
+      <UserReadDetail />
+      <UserUpdateDetail />
+    </div>
+  );
+};
+
+export default UserProfile;
