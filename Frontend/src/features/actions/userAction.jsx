35b412918e8620fc0d3a99f9a9ad5518e0f42778
Amanpreet Singh <amansg421@gmail.com>
2025-06-29 14:10:32 +0530

    created update and delete async  thunk

--- Frontend/src/features/actions/userAction.jsx
+++ Frontend/src/features/actions/userAction.jsx
@@ -18,12 +18,10 @@ export const asyncLoginUser = (user) => async (dispatch) => {
     const { data } = await axios.get(
       `/users?username=${user.username}&password=${user.password}`
     );
-    console.log(data);
     
     if (data && data.length > 0) {
       dispatch(loadUser(data[0])); // ✅ Correct: load the found user
       localStorage.setItem("userCredential", JSON.stringify(data[0]));
-      toast.success("Login successful!");
       return true;
     } else {
       toast.error("Invalid credentials");
@@ -38,7 +36,6 @@ export const asyncLogoutUser = () => async (dispatch) => {
   try {
     localStorage.removeItem("userCredential");
     dispatch(clearUser());
-    toast.success("User logged out successfully!");
   } catch (error) {
     toast.error(error.message || "There is some error!");
     console.error(error);
@@ -48,9 +45,29 @@ export const asyncGetCurrentUser = () => async (dispatch) => {
   try {
     const currentUser = JSON.parse(localStorage.getItem("userCredential"));
     if (currentUser) dispatch(loadUser(currentUser));
-    else console.log("user not logged in!");
   } catch (error) {
     toast.error(error.message || "There is some error!");
     console.error(error);
+  }
+};
+
+export const asyncUpdateUser = (user) => async (dispatch) => {
+  try {
+  const { data } = await axios.patch(`/users/${user.id}`, user);
+  dispatch(asyncLoginUser(data)); 
+  } catch (error) {
+    toast.error(error.message || "Failed to update user.");
+    console.error(error);
+  }
+};
+export const asyncDeleteUser = (id) => async (dispatch) => {
+  try {
+  await axios.delete(`/users/${id}`);
+  dispatch(asyncLogoutUser());
+    return true;
+  } catch (error) {
+    toast.error(error.message || "Failed to delete product.");
+    console.error(error);
+    return false;
   }
 };
